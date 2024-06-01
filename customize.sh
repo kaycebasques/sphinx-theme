9d751cf0e324459f1c1d4104b41300d09f6cfe0e
Kayce Basques <kaycebasques@gmail.com>
2024-06-01 08:33:45 -0700

    Make the starter layout more usable

--- customize.sh
+++ customize.sh
@@ -3,6 +3,7 @@ OLD_NAME="sphinx-theme"
 echo "# $NEW_NAME" > README.md
 sed -i "s/$OLD_NAME/$NEW_NAME/g" setup.py
 sed -i "s/$OLD_NAME/$NEW_NAME/g" $OLD_NAME/__init__.py
+sed -i "s/$OLD_NAME/$NEW_NAME/g" $OLD_NAME/layout.html
 sed -i "s/$OLD_NAME/$NEW_NAME/g" $OLD_NAME/theme.conf
 sed -i "s/$OLD_NAME/$NEW_NAME/g" docs/conf.py
 mv $OLD_NAME/static/$OLD_NAME.css $OLD_NAME/static/$NEW_NAME.css
